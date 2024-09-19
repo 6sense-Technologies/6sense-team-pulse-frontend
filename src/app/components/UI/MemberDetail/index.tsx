7181bdff008bcfb88c6fc49ed9a49f420da86bb3
hasibmuhammad <hasibmu99@gmail.com>
2024-09-19 16:47:12 +0600

    Archive feature added - with user consent

--- src/app/components/UI/MemberDetail/index.tsx
+++ src/app/components/UI/MemberDetail/index.tsx
@@ -1,17 +1,52 @@
-import { Button } from "@/app/components/UI/ButtonComponent"
-import ImageComponent from "@/app/components/UI/ImageComponent"
-import MemberDetailListView from "@/app/components/UI/MemberDetailListView"
-import { cn } from "@/app/utils/tailwindMerge"
-import { IMemberDetail } from "@/types/types"
+"use client";
+import { Button } from "@/app/components/UI/ButtonComponent";
+import ConfirmDialog from "@/app/components/UI/ConfirmDialog";
+import ImageComponent from "@/app/components/UI/ImageComponent";
+import MemberDetailListView from "@/app/components/UI/MemberDetailListView";
+import { BACKEND_URI } from "@/app/utils/constants/constants";
+import { cn } from "@/app/utils/tailwindMerge";
+import { IMemberDetail } from "@/types/types";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface IProps {
     data?: IMemberDetail,
     totalCountAndLimit: { totalCount: number, size: number }
 }
 const MemberDetail = ({ data, totalCountAndLimit }: IProps): JSX.Element => {
-    console.log(data);
+
+    const router = useRouter();
+
+    const [isOpenDialog, setIsOpenDialog] = useState(false);
+
+    const handleCloseDialog = (): void => {
+        setIsOpenDialog(false);
+    };
+
+    const archiveMutation = useMutation({
+        mutationKey: ["archive", data?.accountId],
+        mutationFn: async () => {
+            const res = await axios.put(`${BACKEND_URI}/users/${data?.accountId}/archive`, null);
+            return res.data;
+        }
+    })
+
+    const handleConfirmDelete = (): void => {
+        // Logic for archiving the member goes here
+        console.log("Member archived");
+
+        archiveMutation.mutate(undefined, {
+            onSuccess: () => {
+                router.push("/member-list?page=1");
+                handleCloseDialog();
+            }
+        });
+    };
+
     return (
-        <div className="mt-10">
+        <div className="mt-10 relative">
             <div className="flex flex-col md:flex-row gap-4 md:gap-0 md:justify-between">
                 <div className={cn("flex flex-col md:flex-row items-start md:gap-2", { "md:items-center": !data?.emailAddress })}>
                     <ImageComponent
@@ -27,19 +62,25 @@ const MemberDetail = ({ data, totalCountAndLimit }: IProps): JSX.Element => {
                         <p className="text-sm text-subHeading">
                             {data?.emailAddress}
                         </p>
-                        {/* <p className=" text-sm text-subHeading">
-                            Frontend Engineer
-                        </p> */}
+                        <p className=" text-sm text-subHeading">
+                            {data?.designation}
+                        </p>
                     </div>
                 </div>
                 <div>
-                    <Button prefixIcon="Trash">Archive</Button>
+                    <Button onClick={() => { setIsOpenDialog(true); }} prefixIcon="Trash">Archive</Button>
                 </div>
             </div>
 
             <div className="mt-8">
                 <MemberDetailListView totalCountAndLimit={totalCountAndLimit} data={data?.issueHistory} accountId={`${data?.accountId}`} />
             </div>
+
+
+            {/* Modal */}
+            {isOpenDialog && (
+                <ConfirmDialog isLoading={archiveMutation.isPending} isOpen={isOpenDialog} onClose={handleCloseDialog} onConfirm={handleConfirmDelete} title="Are you sure to archive this member?" />
+            )}
         </div>
     )
 }
