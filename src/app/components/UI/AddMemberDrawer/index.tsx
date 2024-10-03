b55ec01ce9501daddaa256f727657858d60f2cbc
hasibmuhammad <hasibmu99@gmail.com>
2024-10-03 18:40:39 +0600

    Userfrom added to the request body

--- src/app/components/UI/AddMemberDrawer/index.tsx
+++ src/app/components/UI/AddMemberDrawer/index.tsx
@@ -120,7 +120,7 @@ const AddMemberDrawer = ({ isOpen, onClose, refetch }: IProps): JSX.Element => {
 
       // Trello:
       if (newMember.jiraOrTrello && newMember.jiraOrTrello === "trello") {
-        const res = await axios.post(`${BACKEND_URI}/trello/users/create`, { accountId: newMember.trelloId, designation: newMember.designation, project: newMember.project });
+        const res = await axios.post(`${BACKEND_URI}/trello/users/create`, { accountId: newMember.trelloId, designation: newMember.designation, project: newMember.project, userFrom: newMember?.jiraOrTrello});
         return res.data;
       }
     },
