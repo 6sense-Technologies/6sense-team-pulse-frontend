78c9186553105e57473556d55d55d458bae94ba2
hasibmuhammad <hasibmu99@gmail.com>
2024-10-08 16:03:05 +0600

    Added array to prevent duplication on codacy

--- src/app/components/UI/EmptyTableSkeleton/index.tsx
+++ src/app/components/UI/EmptyTableSkeleton/index.tsx
@@ -1,82 +1,40 @@
-import { cn } from "@/app/utils/tailwindMerge"
+import { cn } from "@/app/utils/tailwindMerge";
 
 interface IPropTypes {
-    customClass?: string
-    hideTwoRow?: boolean
+    customClass?: string;
+    hideTwoRow?: boolean;
 }
+
+const SkeletonRow = ({ className, widths }: { className: string; widths: string[] }) => {
+    return (
+        <div className={cn("bg-white grid grid-cols-4 items-center gap-x-10 py-[18px] px-4 border-b-[1px] border-[#F8F9FB]", className)}>
+            {widths.map((width, index) => (
+                <div key={index} className={`max-w-[${width}] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]`}></div>
+            ))}
+        </div>
+    );
+};
 
 const EmptyTableViewSkeleton = ({ customClass, hideTwoRow }: IPropTypes): JSX.Element => {
     return (
         <div data-testid="skeleton-loader" className={cn("bg-white rounded-lg", customClass)}>
             <div className="bg-[#F8F9FB] grid grid-cols-4 items-center gap-x-10 py-3 px-4 border-b-[1px] border-borderColor">
-                <div className="max-w-[112px] bg-[#E5E5E7] rounded-full text-white h-[16px] animate-pulse"></div>
-                <div className="max-w-[112px] bg-[#E5E5E7] rounded-full text-white h-[16px] animate-pulse"></div>
-                <div className="max-w-[112px] bg-[#E5E5E7] rounded-full text-white h-[16px] animate-pulse"></div>
-                <div className="max-w-[112px] bg-[#E5E5E7] rounded-full text-white h-[16px] animate-pulse"></div>
+                {Array(4).fill(null).map((_, index) => (
+                    <div key={index} className="max-w-[112px] bg-[#E5E5E7] rounded-full text-white h-[16px] animate-pulse"></div>
+                ))}
             </div>
-            {/* repeating pattern */}
-            <div className="bg-white grid grid-cols-4 items-center gap-x-10 py-[18px] px-4 border-b-[1px] border-[#F8F9FB]">
-                <div className="max-w-[280px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[145px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[145px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[145px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-            </div>
-            <div className="bg-white grid grid-cols-4 items-center gap-x-10 py-[18px] px-4 border-b-[1px] border-[#F8F9FB]">
-                <div className="max-w-[256px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[125px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[125px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[125px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-            </div>
-            <div className="bg-white grid grid-cols-4 items-center gap-x-10 py-[18px] px-4 border-b-[1px] border-[#F8F9FB]">
-                <div className="max-w-[280px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[145px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[145px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[145px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-            </div>
-            <div className="bg-white grid grid-cols-4 items-center gap-x-10 py-[18px] px-4 border-b-[1px] border-[#F8F9FB]">
-                <div className="max-w-[256px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[125px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[125px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[125px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-            </div>
-            <div className="bg-white grid grid-cols-4 items-center gap-x-10 py-[18px] px-4 border-b-[1px] border-[#F8F9FB]">
-                <div className="max-w-[280px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[145px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[145px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[145px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-            </div>
-            <div className="bg-white grid grid-cols-4 items-center gap-x-10 py-[18px] px-4 border-b-[1px] border-[#F8F9FB]">
-                <div className="max-w-[256px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[125px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[125px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[125px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-            </div>
-            <div className="bg-white grid grid-cols-4 items-center gap-x-10 py-[18px] px-4 border-b-[1px] border-[#F8F9FB]">
-                <div className="max-w-[280px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[145px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[145px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[145px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-            </div>
-            <div className="bg-white grid grid-cols-4 items-center gap-x-10 py-[18px] px-4 border-b-[1px] border-[#F8F9FB]">
-                <div className="max-w-[256px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[125px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[125px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[125px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-            </div>
-            <div className={cn("bg-white grid grid-cols-4 items-center gap-x-10 py-[18px] px-4 border-b-[1px] border-[#F8F9FB]", { "hidden": hideTwoRow })}>
-                <div className="max-w-[280px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[145px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[145px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[145px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-            </div>
-            <div className={cn("bg-white grid grid-cols-4 items-center gap-x-10 py-[18px] px-4 border-b-[1px] border-[#F8F9FB]", { "hidden": hideTwoRow })}>
-                <div className="max-w-[256px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[125px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[125px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-                <div className="max-w-[125px] bg-[#F5F5F5] rounded-full text-pageBg h-[16px]"></div>
-            </div>
-        </div>
-    )
-}
 
-export default EmptyTableViewSkeleton
+            {/* Repeating Skeleton Rows */}
+            <SkeletonRow className="" widths={["280px", "145px", "145px", "145px"]} />
+            <SkeletonRow className="" widths={["256px", "125px", "125px", "125px"]} />
+            <SkeletonRow className="" widths={["280px", "145px", "145px", "145px"]} />
+            <SkeletonRow className="" widths={["256px", "125px", "125px", "125px"]} />
+            <SkeletonRow className="" widths={["280px", "145px", "145px", "145px"]} />
+            <SkeletonRow className="" widths={["256px", "125px", "125px", "125px"]} />
+            <SkeletonRow className={cn({ hidden: hideTwoRow })} widths={["280px", "145px", "145px", "145px"]} />
+            <SkeletonRow className={cn({ hidden: hideTwoRow })} widths={["256px", "125px", "125px", "125px"]} />
+        </div>
+    );
+};
+
+export default EmptyTableViewSkeleton;
