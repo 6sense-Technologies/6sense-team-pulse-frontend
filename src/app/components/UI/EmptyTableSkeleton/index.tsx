e64836ae7554b6d83145aa8b3098f181702633d0
hasibmuhammad <hasibmu99@gmail.com>
2024-10-08 16:34:11 +0600

    resolved arrow-body surrounding issue

--- src/app/components/UI/EmptyTableSkeleton/index.tsx
+++ src/app/components/UI/EmptyTableSkeleton/index.tsx
@@ -5,7 +5,7 @@ interface IPropTypes {
     hideTwoRow?: boolean;
 }
 
-const SkeletonRow = ({ className, widths }: { className: string; widths: string[] }) => {
+const SkeletonRow = ({ className, widths }: { className: string; widths: string[] }): JSX.Element => {
     return (
         <div className={cn("bg-white grid grid-cols-4 items-center gap-x-10 py-[18px] px-4 border-b-[1px] border-[#F8F9FB]", className)}>
             {widths.map((width, index) => (
@@ -19,9 +19,11 @@ const EmptyTableViewSkeleton = ({ customClass, hideTwoRow }: IPropTypes): JSX.El
     return (
         <div data-testid="skeleton-loader" className={cn("bg-white rounded-lg", customClass)}>
             <div className="bg-[#F8F9FB] grid grid-cols-4 items-center gap-x-10 py-3 px-4 border-b-[1px] border-borderColor">
-                {Array(4).fill(null).map((_, index) => (
-                    <div key={index} className="max-w-[112px] bg-[#E5E5E7] rounded-full text-white h-[16px] animate-pulse"></div>
-                ))}
+                {Array(4).fill(null).map((_, index) => {
+                    return (
+                        <div key={index} className="max-w-[112px] bg-[#E5E5E7] rounded-full text-white h-[16px] animate-pulse"></div>
+                    )
+                })}
             </div>
 
             {/* Repeating Skeleton Rows */}
