5044730dcaeda7ac7442fbb8321d2698a9ea34cf
Khan Atik Faisal <khanatik1176@gmail.com>
2025-03-13 12:02:43 +0600

    fixed-some-Minor-UI-issues

--- src/app/(dashboards)/projects/create/page.tsx
+++ src/app/(dashboards)/projects/create/page.tsx
@@ -1,65 +1,73 @@
-"use client";
-import { BaseInput } from "@/components/BaseInput";
-import GlobalBreadCrumb from "@/components/globalBreadCrumb";
-import PageHeading from "@/components/pageHeading";
-import PageTitle from "@/components/PageTitle";
-import { useRouter } from "next/navigation";
-import React from "react";
-import { useForm, useFieldArray } from "react-hook-form";
-import { Button } from "@/components/ButtonComponent";
-import ToolDropdown from "./_components/ToolDropdown";
-import WorkspaceURL from "./_components/WorkspaceURL";
-import { Circle, Trash2 } from "lucide-react";
-import { ProjectTools } from "@/types/Project.types";
-import { useMutation } from "@tanstack/react-query";
-import { CreateProject } from "../../../../../helpers/projects/projectApi";
-import { SidebarTrigger } from "@/components/ui/sidebar";
-import { useSession } from "next-auth/react";
-import AvatarMenu from "@/components/AvatarMenu";
-import { toast } from "@/hooks/use-toast";
+'use client';
+import { BaseInput } from '@/components/BaseInput';
+import GlobalBreadCrumb from '@/components/globalBreadCrumb';
+import PageHeading from '@/components/pageHeading';
+import PageTitle from '@/components/PageTitle';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import { useForm, useFieldArray } from 'react-hook-form';
+import { Button } from '@/components/ButtonComponent';
+import ToolDropdown from './_components/ToolDropdown';
+import WorkspaceURL from './_components/WorkspaceURL';
+import { Circle, Info, Trash2 } from 'lucide-react';
+import { ProjectTools } from '@/types/Project.types';
+import { useMutation } from '@tanstack/react-query';
+import { CreateProject } from '../../../../../helpers/projects/projectApi';
+import { SidebarTrigger } from '@/components/ui/sidebar';
+import { useSession } from 'next-auth/react';
+import AvatarMenu from '@/components/AvatarMenu';
+import { toast } from '@/hooks/use-toast';
+import Modal from '@/components/customModal';
+
 
 const ProjectCreate = () => {
   const router = useRouter();
   const session = useSession();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedTool, setSelectedTool] = useState<string | null>(null);
+
   const {
     handleSubmit,
     control,
     formState: { errors },
     setError,
     clearErrors,
+    getValues,
+    watch,
   } = useForm<ProjectTools>({
     defaultValues: {
-      tools: [{ toolName: "", toolUrl: "" }],
+      tools: [{ toolName: '', toolUrl: '' }],
     },
   });
 
   const { fields, append, remove } = useFieldArray({
     control,
-    name: "tools",
+    name: 'tools',
   });
 
   const handleAddtools = (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ): void => {
     e.preventDefault();
-    append({ toolName: "", toolUrl: "" });
+    append({ toolName: '', toolUrl: '' });
   };
 
   const validateURL = (url: string) => {
-    const urlRegex = /^https:\/\/((?!-)(?!.*--)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}(\/[^\s]*)?$/;
+    const urlRegex =
+      /^https:\/\/((?!-)(?!.*--)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}(\/[^\s]*)?$/;
     return urlRegex.test(url);
   };
 
   const validate = (data: ProjectTools) => {
     let valid = true;
     if (!data.name) {
-      setError("name", {
-        type: "manual",
-        message: "Project name is required.",
+      setError('name', {
+        type: 'manual',
+        message: 'Project name is required.',
       });
       valid = false;
     } else {
-      clearErrors("name");
+      clearErrors('name');
     }
 
     data.tools.forEach((tool, index) => {
@@ -67,8 +75,8 @@ const ProjectCreate = () => {
         // First pair is mandatory
         if (!tool.toolName) {
           setError(`tools.${index}.toolName`, {
-            type: "manual",
-            message: "Minimum one tool must be selected.",
+            type: 'manual',
+            message: 'Minimum one tool must be selected.',
           });
           valid = false;
         } else {
@@ -77,14 +85,14 @@ const ProjectCreate = () => {
 
         if (!tool.toolUrl) {
           setError(`tools.${index}.toolUrl`, {
-            type: "manual",
-            message: "Workspace URL is required.",
+            type: 'manual',
+            message: 'Workspace URL is required.',
           });
           valid = false;
         } else if (!validateURL(tool.toolUrl)) {
           setError(`tools.${index}.toolUrl`, {
-            type: "manual",
-            message: "Invalid URL.",
+            type: 'manual',
+            message: 'Invalid URL.',
           });
           valid = false;
         } else {
@@ -100,8 +108,8 @@ const ProjectCreate = () => {
           valid = false;
         } else if (tool.toolUrl && !validateURL(tool.toolUrl)) {
           setError(`tools.${index}.toolUrl`, {
-            type: "manual",
-            message: "Invalid URL.",
+            type: 'manual',
+            message: 'Invalid URL.',
           });
           valid = false;
         } else {
@@ -110,8 +118,8 @@ const ProjectCreate = () => {
 
         if (!tool.toolName && tool.toolUrl) {
           setError(`tools.${index}.toolName`, {
-            type: "manual",
-            message: "Minimum one tool must be selected.",
+            type: 'manual',
+            message: 'Minimum one tool must be selected.',
           });
           valid = false;
         } else {
@@ -128,19 +136,18 @@ const ProjectCreate = () => {
     onSuccess: () => {
       toast({
         title: 'Project Created!',
-        description:
-            'Your project has been created successfully.',
+        description: 'Your project has been created successfully.',
     });
-      router.push("/projects");
+      router.push('/projects');
     },
     onError: (error) => {
-      if (error.message === "Request failed with status code 409") {
-        setError("name", {
-          type: "manual",
-          message: "Project name already exists.",
+      if (error.message === 'Request failed with status code 409') {
+        setError('name', {
+          type: 'manual',
+          message: 'Project name already exists.',
         });
       }
-    }
+    },
   });
 
   const onSubmit = (data: ProjectTools) => {
@@ -148,7 +155,7 @@ const ProjectCreate = () => {
       const filteredData = {
         ...data,
         tools: data.tools.filter(
-          (tool) => tool.toolName.trim() !== "" && tool.toolUrl.trim() !== ""
+          (tool) => tool.toolName.trim() !== '' && tool.toolUrl.trim() !== ''
         ),
       };
 
@@ -156,135 +163,200 @@ const ProjectCreate = () => {
     }
   };
 
+  
+  const getVideoUrl = (toolName: string) => {
+    switch (toolName.toLowerCase()) {
+      case 'codacy':
+        return 'https://archive.org/download/SampleVideo1280x7205mb/SampleVideo_1280x720_5mb.mp4';
+      case 'jira':
+        return 'https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4';
+      case 'trello':
+        return 'https://archive.org/download/BigBuckBunny_328/BigBuckBunny_512kb.mp4';
+      default:
+        return '';
+    }
+  };
+
+  const handleShieldQuestionClick = (index: number) => {
+    const selectedTool = getValues(`tools.${index}.toolName`);
+    setSelectedTool(selectedTool);
+    setIsModalOpen(true);
+  };
+
+  const isToolName = watch();
+
+
   return (
-    <div className="w-full">
-      <PageTitle title="Create Project • Ops4 Team" />
+    <div className='w-full'>
+      <PageTitle title='Create Project • Ops4 Team' />
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="px-4 lg:pl-[24px] lg:pr-[14px] pt-8 pb-8 w-full">
-        <div className="md:hidden pb-4 flex justify-between items-center">
-          <span className="md:hidden pl-1 "><SidebarTrigger /></span>
+        <div className='w-full px-4 pb-8 pt-8 lg:pl-[24px] lg:pr-[14px]'>
+          <div className='flex items-center justify-between pb-4 md:hidden'>
+            <span className='pl-1 md:hidden'>
+              <SidebarTrigger />
+            </span>
           <AvatarMenu />
         </div>
-        <div className="flex justify-between items-center">
+          <div className='flex items-center justify-between'>
         <GlobalBreadCrumb
-            initialData="Projects"
-            initalLink="/projects"
-            secondayData="Create Project"
-            secondayLink="/projects/create"
+              initialData='Projects'
+              initalLink='/projects'
+              secondayData='Create Project'
+              secondayLink='/projects/create'
           />
-        <span className="hidden md:flex pr-2">
+            <span className='hidden pr-2 md:flex'>
           <AvatarMenu />
           </span>
         </div>
-          <PageHeading title="Create Project" className="pl-[7px] lg:pl-[5px] pt-1" />
+          <PageHeading
+            title='Create Project'
+            className='pl-[7px] pt-1 lg:pl-[5px]'
+          />
 
-          <div className="flex flex-col lg:flex-row items-start  lg:items-center lg:justify-between w-full max-w-[872px] lg:pt-4">
-            <div className="pl-[6px] py-4 lg:pt-0">
-              <h1 className="text-[16px] lg:text-headingXXS font-semibold lg:pb-2">
+          <div className='flex w-full max-w-[872px] flex-col items-start lg:flex-row lg:items-center lg:justify-between lg:pt-4'>
+            <div className='py-4 pl-[6px] lg:pt-0'>
+              <h1 className='text-[16px] font-semibold lg:pb-2 lg:text-headingXXS'>
                 Project Info
               </h1>
             </div>
-            <div className="w-full max-w-[553px] pl-2 lg:pl-0 lg:pt-8">
+            <div className='w-full max-w-[553px] pl-2 lg:pl-0 lg:pt-8'>
               <label
-                htmlFor="projectName"
-                className="text-sm font-medium text-black"
+                htmlFor='projectName'
+                className='text-sm font-medium text-black'
               >
-                Project Name <span className="text-destructive">*</span>
+                Project Name <span className='text-destructive'>*</span>
               </label>
               <BaseInput
                 control={control}
-                name="name"
-                type="text"
-                placeholder="Project Name"
-                className="placeholder:text-subHeading w-full mt-[4px]"
-                additionalText="The project name you used in your code project"
+                name='name'
+                type='text'
+                placeholder='Project Name'
+                className='mt-[4px] w-full placeholder:text-subHeading'
+                additionalText='The project name you used in your code project'
                 errors={errors}
-                errorclassName="mt-1"
+                errorclassName='mt-1'
               />
             </div>
           </div>
 
-          <div className="flex flex-col lg:flex-row items-start lg:items-center lg:justify-between w-full lg:max-w-[872px] mt-8 lg:mt-5">
-            <div className="pl-2 lg:pl-[6px] pt-3 lg:pt-4">
-              <h1 className="text-headingXXS font-semibold pb-1">
+          <div className='mt-8 flex w-full flex-col items-start lg:mt-5 lg:max-w-[872px] lg:flex-row lg:items-center lg:justify-between'>
+            <div className='pl-2 pt-3 lg:pl-[6px] lg:pt-4'>
+              <h1 className='pb-1 text-headingXXS font-semibold'>
                 Project Management Tool
               </h1>
             </div>
             <ToolDropdown
               control={control}
-              name="tools[0].toolName"
-              placeholder="Select"
+              name='tools[0].toolName'
+              placeholder='Select'
               errors={errors.tools?.[0]?.toolName?.message}
               index={0}
             />
           </div>
-          <div className="w-full h-full pl-2 pt-3 lg:pl-[320px]">
+          <div className='flex h-full w-full gap-x-[6px] pl-2 pt-3 lg:pl-[320px]'>
             <WorkspaceURL
               control={control}
-              name="tools[0].toolUrl"
+              name='tools[0].toolUrl'
               errors={errors.tools?.[0]?.toolUrl?.message}
               index={0}
             />
+            {isToolName.tools[0].toolName !== '' ? (
+              <div className='relative mt-[62px] h-9 w-10 rounded-lg lg:mt-[71px]'>
+                <Info
+                  className='absolute right-2 top-2 h-4 w-4 cursor-pointer font-normal text-black md:right-3'
+                  onClick={() => handleShieldQuestionClick(0)}
+                />
+              </div>
+            ) : null}
           </div>
 
           {fields.slice(1).map((field, index) => (
             <div
               key={field.id}
-              className="w-full h-full pl-1 pt-6 lg:pl-[320px] flex-col items-center"
+              className='h-full w-full flex-col items-center pl-1 pt-6 lg:pl-[320px]'
             >
-              <div className="flex items-end justify-start gap-x-[6px]">
+              <div className='flex items-end justify-start gap-x-[6px]'>
                 <ToolDropdown
                   control={control}
                   name={`tools[${index + 1}].toolName`}
                   errors={errors.tools?.[index + 1]?.toolName?.message}
                   index={index + 1}
                 />
-                <div className="relative border w-10 h-9 rounded-lg">
+                <div className='relative h-9 w-10 rounded-lg border'>
                   <Trash2
-                    className="w-4 h-4 text-black font-normal cursor-pointer absolute top-2 right-2 md:right-3"
+                    className='absolute right-2 top-2 h-4 w-4 cursor-pointer font-normal text-black md:right-3'
                     onClick={() => remove(index + 1)}
                   />
                 </div>
               </div>
-              <div className="flex items-center gap-4 pl-2 pt-4 lg:pl-0 lg:pt-0">
+              <div className='flex items-center gap-2 pl-2 pt-4 lg:pl-0 lg:pt-0'>
                 <WorkspaceURL
                   control={control}
                   name={`tools[${index + 1}].toolUrl`}
                   errors={errors.tools?.[index + 1]?.toolUrl?.message}
                   index={index + 1}
                 />
+                {isToolName.tools[index + 1].toolName !== '' ? (
+                  <div className='relative mt-[62px] h-9 w-10 rounded-lg lg:mt-[71px]'>
+                    <Info
+                      className='absolute right-4 top-2 h-4 w-4 cursor-pointer font-normal text-black md:right-3'
+                      onClick={() => handleShieldQuestionClick(index + 1)}
+                    />
+                  </div>
+                ) : null}
               </div>
             </div>
           ))}
 
-          <div className="mt-14 mb-10 ml-2 lg:ml-[320px] flex-col items-start lg:items-center">
-            <div className="w-full">
+          <div className='mb-10 ml-2 mt-14 flex-col items-start lg:ml-[320px] lg:items-center'>
+            <div className='w-full'>
             <Button
-              variant="extralight"
-              size="xsExtended"
+                variant='extralight'
+                size='xsExtended'
               onClick={handleAddtools}
-              className="mb-6 md:max-w-[90px]"
+                className='mb-6 md:max-w-[90px]'
             >
               Add Tool
             </Button>
             </div>
-            <div className="w-full">
+            <div className='w-full'>
             <Button
-              variant="darkish"
-              size="lgExtended"
-              type="submit"
-              className="mt-3 w-full md:max-w-[128px]"
+                variant='darkish'
+                size='lgExtended'
+                type='submit'
+                className='mt-3 w-full md:max-w-[128px]'
             >
               {projectMutation.isPending ? (
-                <Circle className="animate-spin" size={14} />
+                  <Circle className='animate-spin' size={14} />
               ) : (
-                "Create Project"
+                  'Create Project'
               )}
             </Button>
             </div>
           </div>
         </div>
       </form>
+
+      {/* Modal for video */}
+      {isModalOpen && (
+        <Modal toolName={selectedTool}>
+          <div>
+            <video controls className='mt-4 w-full'>
+              <source src={getVideoUrl(selectedTool || '')} type='video/mp4' />
+              Your browser does not support the video tag.
+            </video>
+            <div className='mb-3 mt-8 flex justify-end'>
+              <Button
+                variant='lightex'
+                size='xsExtended'
+                onClick={() => setIsModalOpen(false)}
+              >
+                Close
+              </Button>
+            </div>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 };
---
+++ src/components/customModal.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+interface ModalProps {
+  onClose?: () => void;
+  children: React.ReactNode;
+  toolName: string | null;
+}
+
+const capitalizeFirstLetter = (string: string | null) => {
+  if (string === null) return '';
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+const Modal: React.FC<ModalProps> = ({ children, toolName }) => {
+  return (
+    <div className='fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50'>
+      <div className='w-full max-w-[600px] rounded-lg bg-white shadow-lg'>
+        <div className='flex justify-between px-4 pt-6'>
+          <div className='flex items-center'>
+            <h2 className='text-lg font-semibold'>Finding your workspace URL</h2>
+            <span className='ml-2 bg-white text-black text-xs px-2.5 py-0.5 rounded-3xl border font-normal'>
+              {capitalizeFirstLetter(toolName)}
+            </span>
+          </div>
+        </div>
+        <div className='px-4 pb-4'>
+          <p className='mt-2 text-sm text-gray-600'>
+            Follow these simple steps to locate your workspace URL quickly and easily.
+          </p>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
