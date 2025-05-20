21d1d18634486a214b3708a1af1d6d90a7fe0b65
Ashikul Islam Nayeem <ain.nayeem1999@gmail.com>
2025-05-20 16:38:53 +0600

    git commit check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-import MenuComponent from '@/app/components/UI/MenuComponent';
-import PageTitle from '@/app/components/UI/PageTitle';
-import { redirect } from 'next/navigation';
+import MenuComponent from "@/app/components/UI/MenuComponent";
+import PageTitle from "@/app/components/UI/PageTitle";
+import { redirect } from "next/navigation";
 export default function Home(): JSX.Element {
-
-  redirect("/member-list?page=1")
+  redirect("/member-list?page=1");
 
   return (
-    <div className='relative adjustedWidthForMenu px-6 md:left-[280px]'>
-      <MenuComponent currentPage={'home'} />
-      <PageTitle pageName='Home' title='6sense Efficiency' />
+    <div className="relative adjustedWidthForMenu px-6 md:left-[280px]">
+      <MenuComponent currentPage={"home"} />
+      <PageTitle pageName="Home" title="6sense Efficiency" />
     </div>
   );
 }
