1a5629ac98657c7ad5fbe666c673547facf6e271
ainayeem <ain.nayeem1999@gmail.com>
2025-07-08 11:50:30 +0600

    feat: :rocket: build check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-
-import PageTitle from '@/components/PageTitle';
-import { redirect } from 'next/navigation';
+import PageTitle from "@/components/PageTitle";
+import { redirect } from "next/navigation";
 export default function Home(): JSX.Element {
-
   redirect("/sign-in");
 
   return (
-    <div className='relative adjustedWidthForMenu px-6 md:left-[280px]'>
+    <div className="relative adjustedWidthForMenu px-6 md:left-[280px]">
       {/* <MenuComponent currentPage={'home'} /> */}
-      <PageTitle pageName='Home' title='6sense Efficiency' />
+      <PageTitle pageName="Home" title="6sense Efficiency" />
     </div>
   );
 }
+// deploy check
+//
